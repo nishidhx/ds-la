caf226360079dc7d6ad9ee166f15a6ca196558b3
nishidhx <xnishidh.codes@gmail.com>
2026-08-25 17:00:11 +0530

    dangling pointer + other prs

--- 25_/main.c
+++ 25_/main.c
@@ -60,8 +60,124 @@ int pr3() {
     return 0;
 }
 
+// create a dynamic array and reverse the actual array instead of printing the reverse of the array
+// I solved by two pointer Approach -> same did by sir but used while loop
+// while loop code
+/**
+    int i = 0, j = number_of_elements - 1;
+
+    while 
+    
+    
+
+*/
+
+int pr4() {
+    int number_of_elements;
+    printf("Enter number of elements: ");
+    scanf("%d", &number_of_elements);
+
+    int *arr = malloc(number_of_elements * sizeof(int));
+
+    for (int i = 0; i < number_of_elements; i++) {
+        printf("Enter element %d: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array
+    for (int i = 0; i < number_of_elements / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[number_of_elements - 1 - i];
+        arr[number_of_elements - 1 - i] = temp;
+    }
+
+    printf("Reversed array: ");
+    for (int i = 0; i < number_of_elements; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+
+// pr5 -> create the dynamic array and count how many are even and odd.
+int isEven(int number) {
+    return (number & 1) == 0;
+}
+
+int pr5() {
+    int number_of_elements, e_c = 0, o_c = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &number_of_elements);
+
+    int *arr = malloc(number_of_elements * sizeof(int));
+
+    for (int i = 0; i < number_of_elements; i++) {
+        printf("Enter element %d: ", i);
+        scanf("%d", &arr[i]);
+        if (isEven(arr[i])) {
+            e_c++;
+        }else {
+            o_c++;
+        }
+    }
+
+    printf("even count: %d, odd count: %d", e_c, o_c);
+    return 0;
+}
+
+// create a dynamic array and take avg of the array use calloc
+int pr6() {
+    int number_of_elements, e_c = 0, o_c = 0, avg = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &number_of_elements);
+
+    int *arr = calloc(number_of_elements, sizeof(int));
+
+    for (int i = 0; i < number_of_elements; i++) {
+        printf("Enter element %d: ", i);
+        scanf("%d", &arr[i]);
+        avg += arr[i];
+    }
+
+    printf("Aveerage: %d", avg/number_of_elements);
+
+    return 0;
+
+}
+
+// create a dynamic array using calloc print array first without init and after init
+int pr7() {
+    int number_of_elements;
+    printf("Enter number of elements: ");
+    scanf("%d", &number_of_elements);
+
+    int *arr = calloc(number_of_elements, sizeof(int));
+
+    for (int i = 0; i < number_of_elements; i++) {
+            printf("%d ", arr[i]);
+    }
+
+    for (int i = 0; i < number_of_elements; i++) {
+        printf("Enter element %d: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < number_of_elements; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+// memory leak - free() function
+// dangling pointer - that pointer variable we store an address of the a component which don't exist.
+/**
+    what is dangling Pointer ?
+    free(arr);
+    arr = NULL;
+*/
 
 int main() {
 
-    pr3();
+    pr7();
 }
