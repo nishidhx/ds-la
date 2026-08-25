db6cf912b79d0f80fff9b7be9ac2c3fcbd46d237
nishidhx <xnishidh.codes@gmail.com>
2026-08-25 10:22:51 +0530

    pr2 and pe3

--- 25_/main.c
+++ 25_/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+/** Dynamic Memory Allocation */
+
+// first program of dynamic memory allocation
+int pr1(){
     int n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
@@ -16,4 +19,49 @@ int main() {
     }
 
     return 0;
+
+}
+
+// sum of elements from the dynamic array
+int pr2() {
+    int number_of_elements, sum = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &number_of_elements);
+
+    int *arr = malloc(number_of_elements * sizeof(int));
+
+    for (int i = 0; i < number_of_elements; i++) {
+        printf("Enter element %d: ", i);
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Sum: %d", sum);
+    return 0;
+}
+
+// create a dynamic array and find out the max element
+int pr3() {
+    int number_of_elements, max = -99999;
+    printf("Enter number of elements: ");
+    scanf("%d", &number_of_elements);
+
+    int *arr = malloc(number_of_elements * sizeof(int));
+
+    for (int i = 0; i < number_of_elements; i++) {
+        printf("Enter element %d: ", i);
+        scanf("%d", &arr[i]);
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+
+    printf("max: %d", max);
+    return 0;
+}
+
+
+int main() {
+
+    pr3();
 }
